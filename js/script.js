fddb484b72e54916c1f3c93619adaca1ba435cfd
Dayronaguilera <aguilera.dayron@gmail.com>
2021-07-04 22:46:10 +0200

    End mileston 4

--- js/script.js
+++ js/script.js
@@ -6,28 +6,6 @@ new Vue(
         data: {
             contacts: [
                 {
-                    name: 'Michele',
-                    avatar: "./img/avatar_1.jpg",
-                    visible: true,
-                    messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            text: 'Hai portato a spasso il cane?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            text: 'Ricordati di dargli da mangiare',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 16:15:22',
-                            text: 'Tutto fatto!',
-                            status: 'received'
-                        }
-                    ],
-                },
-                {
                     name: 'Fabio',
                     avatar: "./img/avatar_2.jpg",
                     visible: true,
@@ -50,6 +28,73 @@ new Vue(
                     ],
                 },
                 {
+                    name: 'Luisa',
+                    avatar: "./img/avatar_6.jpg",
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'gabriele',
+                    avatar: "./img/avatar_4.jpg",
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            text: 'Allora finto subito come sempre?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            text: 'Quasi finito, tu come stai messo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            text: 'Sei sempre il solito secchione BRAVOH, io in alto mare!',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Tony',
+                    avatar: "./img/avatar_5.jpg",
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            text: "WE WE T'appost",
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            text: "Frate T'appost",
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            text: "Che dici finito l'esercizio",
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 16:25:10',
+                            text: "Quasi!",
+                            status: 'sent'
+                        },
+                    ],
+                },
+                
+                {
                     name: 'Samuele',
                     avatar: "./img/avatar_3.jpg",
                     visible: true,
@@ -70,30 +115,27 @@ new Vue(
                             status: 'received'
                         }
                     ],
-                },
-                {
-                    name: 'Luisa',
-                    avatar: "./img/avatar_io.jpg",
-                    visible: true,
-                    messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            text: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            text: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
-                        }
-                    ],
-                },
-                
-                
+                }, 
             ],
+
             indexContacts:0,
             indexMessages:"",
+            search: '',
             
+        },
+        computed: {
+            filteredAndSorted(){
+             // function to compare names
+             function compare(a, b) {
+               if (a.name < b.name) return -1;
+               if (a.name > b.name) return 1;
+               return 0;
+             }
+              
+             return this.contacts.filter(user => {
+                return user.name.toLowerCase().includes(this.search.toLowerCase())
+             }).sort(compare)
+            }
         },
         methods: {
             
@@ -124,6 +166,7 @@ new Vue(
                 });
 
                 this.indexMessages = '';
+
                 // dopo 1 secondo, pushare una riposta.
                 setTimeout(() => {
                     this.contacts[this.indexContacts].messages.push({
@@ -139,5 +182,36 @@ new Vue(
     }    
                 
 );
-  
+
+
+/*
+{
+                    name: 'Noemi',
+                    avatar: "./img/avatar_io.jpg",
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Sei operativa per un pò di codice?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma fa molto caldo non so quanto riesco!',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 16:00:00',
+                            text: "OK, dai proviamoci dobbiamo finire l'esercizio",
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:05:00',
+                            text: 'Tra 10 minuti ci vediamo su Zoom',
+                            status: 'received'
+                        },
+                    ],
+                },
+*/
+
 
